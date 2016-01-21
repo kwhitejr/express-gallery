b70cb3810dca648d1bcbbafa92cff02ed9d34f19
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-20 19:54:29 -1000

    body parser installed, required, and used

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('bodyParser');
 
 var db = require('./models');
 var PORT = 3000;
+var Photo = db.Photo;
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/', function (req, res) {
+  var data =
+  res.render('index', data);
 });
 
 db.sequelize
