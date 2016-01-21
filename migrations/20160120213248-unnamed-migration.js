15a2100c438baac4e257e36a3270019d0da0b310
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-20 21:38:08 -1000

    index renders; first migration defined

---
+++ migrations/20160120213248-unnamed-migration.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      link: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Photos');
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 
 var db = require('./models');
 var PORT = 3000;
@@ -14,13 +14,18 @@ app.set('view engine', 'jade');
 app.set('views', 'views');
 
 app.get('/', function (req, res) {
-  var data = [
-    {author: 'Kevin', link: 'http://lorempixel.com/400/200/nature', description: 'This is a nature.'},
-    {author: 'Ben', link: 'http://lorempixel.com/400/200/transport', description: 'This is a transport.'},
-    {author: 'John', link: 'http://lorempixel.com/400/200/cats', description: 'This is a cats.'}
-  ];
+  var data = {
+    photos: [
+      {author: 'Kevin', link: 'http://lorempixel.com/400/200/nature', description: 'This is a nature.'},
+      {author: 'Ben', link: 'http://lorempixel.com/400/200/transport', description: 'This is a transport.'},
+      {author: 'John', link: 'http://lorempixel.com/400/200/cats', description: 'This is a cats.'},
+      {author: 'Fred', link: 'http://lorempixel.com/400/200/animals', description: 'This is a animals.'}
+    ]
+  };
   res.render('index', data);
 });
+
+
 
 db.sequelize
   .sync()
