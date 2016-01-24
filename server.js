3cfd63ab442fa3c5b0b92cab10183ff06be9b1a4
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-23 18:11:04 -1000

    various forms created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var db = require('./models');
 var PORT = 3000;
@@ -9,9 +10,19 @@ var Photo = db.Photo;
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'jade');
 app.set('views', 'views');
+
+app.put('/gallery/:id', function (req, res) {
+  res.render('put-form');
+});
+
+app.delete('/gallery/:id', function (req, res) {
+  res.render('delete-form');
+});
+
 
 app.get('/', function (req, res) {
   Photo.findAll()
@@ -21,13 +32,14 @@ app.get('/', function (req, res) {
 });
 
 app.get('/gallery/new', function (req, res) {
-  res.render('new', {});
+  res.render('new-form', {});
 });
 
 app.get('/gallery/:id', function (req, res) {
   Photo.find({where: {id: req.params.id}})
     .then(function (result) {
       var locals = {
+        id:          result.id,
         author:      result.author,
         link:        result.link,
         description: result.description
@@ -36,7 +48,12 @@ app.get('/gallery/:id', function (req, res) {
     });
 });
 
-
+app.post('/gallery', function (req, res) {
+  Photo.create(req.body)
+    .then(function (result) {
+      res.redirect('/gallery/'+result.id);
+    });
+});
 
 db.sequelize
   .sync()
